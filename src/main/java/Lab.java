02ddf0a5599b8a75cfc279c675b2fbf5d4f217c5
JitalSoni <Jits2096@gmail.com>
2023-02-24 03:54:59 +0000

    While Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -37,7 +37,14 @@ public class Lab {
         int i = 0;
 
 //        code here
-
+        while(i < end){
+            sum = sum +i;
+            i = i + 1;
+            
+        }
+        
         return sum;
+        
     }
+    
 }
